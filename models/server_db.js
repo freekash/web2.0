f801fb5e37249e7e3761078139ccff276dc3f5cc
freekash@gmail.com <freekash@gmail.com>
2021-01-05 11:04:52 +0530

    GETTING DATA BACK

--- models/server_db.js
+++ models/server_db.js
@@ -8,6 +8,15 @@ save_user_information = (data) => new Promise((resolve,reject)=>{
     resolve('Succesful');
   });
 })
+
+get_total_amount = (data) => new Promise((resolve,reject)=>{
+  db.query('select sum(amount) as total_amount from lottery_information', null, function(err,results, fields){
+    if(err){
+      reject('could not get total amount');
+    }
+    resolve(results);
+  });
+})
 module.exports = {
   save_user_information
 }
