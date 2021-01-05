8054eb437db23e6e42d1404e8e73497bb8eb6441
freekash@gmail.com <freekash@gmail.com>
2021-01-05 10:36:32 +0530

    ADDED DATABASE

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express=require('express');
 const app=express();
 const bodyParser = require('body-parser');
+const {save_user_information}=require('./models/server_db');
+
 
 app.use(bodyParser.json());
 //console.log("hello");
-app.post('/',(req,res)=>{
+app.post('/',async (req,res)=>{
   var email=req.body.email;
   var amount=req.body.amount;
+
  if(amount<=1){
    return_info={};
    return_info.error=true;
    return_info.message="The amount should be greater than 1";
    return res.send(return_info);
  }
-  res.send({"amount":amount,"email":email});
+ var result=await save_user_information({"amount":amount,"email":email});
+  res.send(result);
 });
 app.listen(3000,()=>{
   console.log('server is running on port 3000');
